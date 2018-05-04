587f7f1c4e5af7432d9b9f832c7198509fbacf3b
28707699@qq.com <28707699@qq.com>
2018-05-04 16:51:55 +0800

    鍒犻櫎

--- 我的招投标/rp原型设计/内蒙古建筑业信息采集、审核、归档平台_files/axurerp_pagespecificscript.js
+++
@@ -1,7 +0,0 @@
-﻿for(var i = 0; i < 1; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
-
-$axure.eventManager.pageLoad(
-function (e) {
-
-});
-gv_vAlignTable['u0'] = 'top';
